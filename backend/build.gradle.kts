44e38c7f14046bd6f00d707acb34fbd693e5a90c
nurbolat <djumadilovnurik9@gmail.com>
2026-06-25 18:38:05 +0500

    feat: add flyway + spring data jpa dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -5,8 +5,11 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
 
     runtimeOnly("org.postgresql:postgresql")
 
